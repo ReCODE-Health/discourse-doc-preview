e78c7e14916ede4f9c41931c901679eae99bdc2d
Jarek Radosz <jradosz@gmail.com>
2022-08-14 11:06:11 +0200

    Avoid using to-be-deprecated `mobile` import
    Accessing `mobileView` through the `Site` service is preferred.

--- javascripts/discourse/initializers/initialize-for-doc-preview.js
+++ javascripts/discourse/initializers/initialize-for-doc-preview.js
@@ -1,14 +1,14 @@
 import {withPluginApi} from "discourse/lib/plugin-api";
 import {iconHTML} from "discourse-common/lib/icon-library";
-import Mobile from "discourse/lib/mobile";
 
 const PREVIEW_HEIGHT = 500;
 
 export default {
   name: "doc-previews",
-  initialize() {
+  initialize(container) {
     withPluginApi("0.8.41", (api) => {
-      if (Mobile.mobileView) return;
+      const site = container.lookup("service:site");
+      if (site.mobileView) return;
 
       try {
         const previewModeSetting = settings.preview_mode;
